d860a5964c0c5d09c798733c3753be1df428c71b
Burlaca Artiom <artiomburlaca@gmail.com>
2025-11-20 04:17:52 +0200

    test bacend conection

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,7 @@
 export const BACKEND_URL = "http://72.62.33.22:8080"; // modifică portul dacă backendul tău rulează pe altceva
 
 export async function checkBackend() {
-  const resp = await fetch(`${BACKEND_URL}/health`);
+  const resp = await fetch(`${BACKEND_URL}/api/test/health`);
   if (!resp.ok) throw new Error("Backend error!");
   return resp.json ? await resp.json() : {};
 }
